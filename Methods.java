0600d2e824e7b034dcd7f9518b8ab1b29d379beb
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2024-12-27 00:45:22 +0400

    print() added

--- Methods.java
+++ Methods.java
@@ -1,4 +1,14 @@
 package FOP_Team_Project;
 
+import java.util.Map;
 public class Methods {
+    // Method to print the value of a variable
+    public static Object printVariable(String varName, Map<String, Object> variables) {
+        Object value = variables.get(varName);
+        if (value != null) {
+            return value; // Return the value to be printed
+        } else {
+            return "Error: Variable '" + varName + "' is not defined.";
+        }
+    }
 }
