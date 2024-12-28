a19892462ecb94fe3ce2b7864de53351b155931c
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2024-12-29 01:10:04 +0400

    Multiplication table algorithm

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,4 +1,11 @@
 package FOP_Team_Project;
 
 public class MultiplicationTable {
+
+    // Method to print the multiplication table up to 10 for a given number
+    public void printTable(int number) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
 }
