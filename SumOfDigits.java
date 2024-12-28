a2575b5e744d0f2b37da2d9de7a063c2ba53f28c
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2024-12-29 01:00:43 +0400

    Sum of digits algorithm file

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,4 +1,17 @@
 package FOP_Team_Project;
 
 public class SumOfDigits {
+
+    // Method to calculate the sum of digits in a number
+    public int sumOfDigits(int number) {
+        number = Math.abs(number);  // Ignore the negative sign by using absolute value
+        int sum = 0;
+
+        while (number > 0) {
+            sum += number % 10;  // Add the last digit to the sum
+            number /= 10;         // Remove the last digit
+        }
+
+        return sum;
+    }
 }
