01e48b35f52fd1537e10b8ff81b5b88fec0fdf30
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2024-12-29 00:08:41 +0400

    ReverseNumber algorithm code here

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,4 +1,15 @@
 package FOP_Team_Project;
 
 public class ReverseNumber {
+
+    // Method to reverse the digits of a number
+    public int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
 }
