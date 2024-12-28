39f8f00831ef541a370fad76617c9a08b90fffd3
MurtazBabunashvili <babunashvili.murtaz@kiu.edu.ge>
2024-12-29 01:16:38 +0400

    Fibonacci algorithm here

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,4 +1,19 @@
 package FOP_Team_Project;
 
-public class FibonacciNumber {
+public class Fibonacci {
+
+    // Method to calculate the Nth Fibonacci number using iteration
+    public int calculateFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
